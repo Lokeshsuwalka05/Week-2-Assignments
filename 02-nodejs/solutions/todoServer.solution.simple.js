bdebd67dcfbb556a8608d39f1caf30089da2483a
Lokeshsuwalka05 <lokeshsuwalka2002@gmail.com>
2024-06-15 15:37:43 +0530

    done

--- 02-nodejs/solutions/todoServer.solution.simple.js
+++ 02-nodejs/solutions/todoServer.solution.simple.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path=require("path");
 
 const app = express();
 
@@ -26,14 +27,7 @@ app.get('/todos', (req, res) => {
   res.json(todos);
 });
 
-app.get('/todos/:id', (req, res) => {
-  const todoIndex = findIndex(todos, parseInt(req.params.id));
-  if (todoIndex === -1) {
-    res.status(404).send();
-  } else {
-    res.json(todos[todoIndex]);
-  }
-});
+
 
 app.post('/todos', (req, res) => {
   const newTodo = {
@@ -45,16 +39,7 @@ app.post('/todos', (req, res) => {
   res.status(201).json(newTodo);
 });
 
-app.put('/todos/:id', (req, res) => {
-  const todoIndex = findIndex(todos, parseInt(req.params.id));
-  if (todoIndex === -1) {
-    res.status(404).send();
-  } else {
-    todos[todoIndex].title = req.body.title;
-    todos[todoIndex].description = req.body.description;
-    res.json(todos[todoIndex]);
-  }
-});
+
 
 app.delete('/todos/:id', (req, res) => {
   const todoIndex = findIndex(todos, parseInt(req.params.id));
@@ -66,9 +51,17 @@ app.delete('/todos/:id', (req, res) => {
   }
 });
 
-// for all other routes, return 404
-app.use((req, res, next) => {
-  res.status(404).send();
-});
+// // for all other routes, return 404
+// app.use((req, res, next) => {
+//   res.status(404).send();
+// });
+app.get("/",(req,res)=>{
+  res.sendFile(path.join(__dirname,"index.html"));
 
-module.exports = app;
+})
+
+
+app.listen(3000);
+
+// module.exports = app; 
+ 
